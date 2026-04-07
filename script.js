c748c0d0b17d7d88e42462615bfc7fe6b7f1d370
Pratikshamirje <mirjeprisha@gmail.com>
2026-04-07 10:15:09 +0530

    Added movie images and hover effect

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const movies = [
-  { title: "Movie 1" },
-  { title: "Movie 2" }
+  {
+    title: "Pushpa",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    title: "RRR",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    title: "KGF",
+    image: "https://via.placeholder.com/150"
+  }
 ];
-
 const container = document.getElementById("movies");
 
 movies.forEach(movie => {
   const div = document.createElement("div");
-  div.innerText = movie.title;
+
+  const img = document.createElement("img");
+  img.src = movie.image;
+
+  const title = document.createElement("p");
+  title.innerText = movie.title;
+
+  div.appendChild(img);
+  div.appendChild(title);
+
   container.appendChild(div);
 });
